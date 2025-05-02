1b9457999db8d3795d6e3452a2c757cacdad4464
gabrielgomez20132 <gabrielgomez_100@hotmail.com>
2025-05-01 23:20:18 -0300

    limit en paginations

--- src/helpers/pagination.js
+++ src/helpers/pagination.js
@@ -7,7 +7,7 @@
  */
 const paginate = async (req, getData, getTotal) => {
     const page = parseInt(req.query.page) || 1;
-    const limit = parseInt(req.query.limit) || 10;
+    const limit = parseInt(req.query.limit) || 9;
     const skip = (page - 1) * limit;
 
     const [data, totalRecords] = await Promise.all([
